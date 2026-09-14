898da894071bda0616c54f572c74e8fb87db57ed
Marsi Márk Mihály <marsi.mark.mihaly@petrik.hu>
2026-09-14 13:04:58 +0200

    09.14

---
+++ 09.14/adatok_tarolasa.py
@@ -0,0 +1,28 @@
+print("Hello, ma az adatok tárolását tanuljuk meg :)")
+
+# tipusok: egész, valós, szöveges, karakterek, logikai
+
+nev = "Márk" 
+nev = "Márk"
+print(f"Hello {nev}!")
+print(type(nev)) #tipusa: szöveg ---> string!
+
+egesz = 16 # egész
+print(f"Az egész változó tipusa: {type(egesz)}, értéke pedig: {egesz}")
+
+valos = 32.5 # valos, tizedespont nem vessző
+print(f"Az valós változó tipusa: {type(valos)}, értéke pedig: {valos}")
+
+logikai = True
+print(f"Az logika változó tipusa: {type(logikai)}, értéke pedig: {logikai}")
+
+karakter = '*'
+print(f"Az karakter változó tipusa: {type(karakter)}, értéke pedig: {karakter}")
+
+print("\n********")
+print(f"* {nev} *")
+print("********")
+
+
+
+
---
+++ 09.14/gyakorlas_valtozokkal.py
@@ -0,0 +1,15 @@
+#8. feladat
+a = 5
+b = 10
+print(f"a={a}, b={b}")
+
+print(f"a={a*2}, b={b*2}")
+
+#operátorok
+#aritmetikai operátor: műveleti jelek: + - * / //-egész osztás, % - maradékos osztás
+#értékadó operátor: =
+#összevont értékadó operátor: += -= *= /= %= //=
+
+
+print(10%3)#csak a maradékot adja vissza
+print(10//3)#csak az egész részt adja vissza, levágja a tizedesjegyeket
